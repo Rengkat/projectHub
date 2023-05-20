12823884c849ed24110484b68c96198eb2f44e3b
Rengkat <successinmotion11@gmail.com>
2023-05-20 02:16:57 +0100

    adjustment

--- pages/api/comments/index.ts
+++ pages/api/comments/index.ts
@@ -7,15 +7,12 @@ interface Comment {
   institution: string;
   comment: string;
 }
-//
 
 const client = createClient({
-  projectId: process.env.SANITY_PROJECT_ID, //"igg8dt7d",
-  dataset: process.env.SANITY_DATASET, //"production",
-  useCdn: true, // set to `false` to bypass the edge cache
-  apiVersion: process.env.SANITY_API_VERSION, //"2021-10-21", // use current date (YYYY-MM-DD) to target the latest API version
-  token: process.env.SANITY_API_TOKEN,
-  // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
+  useCdn: true,
+  apiVersion: process.env.SANITY_API_VERSION,
 });
 export default async function createComment(
   req: NextApiRequest,
